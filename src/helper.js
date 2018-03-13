ca7c77fe96b68be63e5d05bb7233a76b2799fee2
Ngoc Phan <nphan4@yahoo.com>
2018-03-12 19:36:05 -0600

    Add findByName function to helper.js

--- src/helper.js
+++ src/helper.js
@@ -5,11 +5,22 @@ export default class DistrictRepository {
 
   cleanData = (stats) => {
     return stats.reduce((cleanObj, district) => {
-      if (!cleanObj[district.Location]) {
-        cleanObj[district.Location] = {};
+      if (!cleanObj[district.Location.toUpperCase()]) {
+        cleanObj[district.Location.toUpperCase()] = {
+          location: district.Location.toUpperCase(),
+          data: {}
+        };
       }
-      cleanObj[district.Location][district.TimeFrame] = district.Data;
+      cleanObj[district.Location.toUpperCase()].data[district.TimeFrame] = district.Data;
       return cleanObj; 
     }, {});
+  }
+
+  findByName = (name = '') => {
+    let keys = Object.keys(this.stats)
+    // console.log(this.stats)
+     const match = keys.find(key => key === name.toUpperCase())
+    //  console.log(match)
+     return this.stats[match];
   }
 }
